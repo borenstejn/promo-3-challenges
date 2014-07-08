ef3ae188d9f7005a247fcd512acfbe250fbcbe0f
borenstejn <jeromeborenstejn@gmail.com>
2014-07-08 16:13:59 +0200

    résolution exercice 4

--- 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/coach_answer.rb
+++ 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/coach_answer.rb
@@ -1,8 +1,22 @@
 def coach_answer(your_message)
-  # TODO: return coach answer to your_message
+	if your_message == "I am going to work right now SIR !"
+		return ""
+	elsif your_message.include? "?"
+		return "Silly question, get dressed and go to work !"
+	else
+		return "I don't care son, get dressed and go to work !"
+	end
 end
 
 def coach_answer_enhanced(your_message)
-  # TODO: return coach answer to your_message, with additional custom rules of yours !
+  if your_message == "I am going to work right now SIR !"
+		return ""
+	elsif your_message.downcase.include? "sir"
+		return "at least you're calling me Sir you piece of shit"
+	elsif your_message.include? "?"
+		return "Silly question, get dressed and go to work !"
+		else return "I don't care son, get dressed and go to work !"
+	end
 end
 
+  # TODO: return coach answer to your_message, with additional custom rules of yours !
--- 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/interface.rb
+++ 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/interface.rb
@@ -1,3 +1,8 @@
 require_relative "coach_answer"
 #TODO: Implement the program that makes you discuss with your coach from the terminal.
-
+text = ""
+until text == "I am going to work right now SIR !"
+	puts "WHAT DO YOU HAVE TO SAY ?"
+	text = gets.chomp
+	puts coach_answer(text)
+end
