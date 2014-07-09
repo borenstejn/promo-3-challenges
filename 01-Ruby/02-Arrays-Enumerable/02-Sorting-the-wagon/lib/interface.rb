0bd5e398a7ecbe4b79ccf68a2ee0235181eaa029
borenstejn <jeromeborenstejn@gmail.com>
2014-07-09 13:00:50 +0200

    excercie wagon en cours

--- 01-Ruby/02-Arrays-Enumerable/02-Sorting-the-wagon/lib/interface.rb
+++ 01-Ruby/02-Arrays-Enumerable/02-Sorting-the-wagon/lib/interface.rb
@@ -6,9 +6,23 @@ begin
   puts students.empty? ? "Type a student name:" : "Type another student name (or press enter to finish):"
   name = gets
   name.chomp! if name
+  students << name if name!=""
 
   # TODO: Add the name we just gotto the students array
 
 end while name != ""
+
+wagon_sort(students)
+effectif = students.count
+studentsbis = students
+studentsbis.delete_at(effectif-1)
+
+
+studentsbis.each do |name|
+	print "#{name}, "
+end
+
+print students[effectif-1]
+
 
 # TODO: Call `wagon_sort` method and display the sorted student list
