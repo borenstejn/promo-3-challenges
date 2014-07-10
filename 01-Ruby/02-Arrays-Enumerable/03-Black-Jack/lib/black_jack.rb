2015e99a8d9de5d52b1d3d303de01f15b65ed260
borenstejn <jeromeborenstejn@gmail.com>
2014-07-10 16:01:59 +0200

    exercice à finir

--- 01-Ruby/02-Arrays-Enumerable/03-Black-Jack/lib/black_jack.rb
+++ 01-Ruby/02-Arrays-Enumerable/03-Black-Jack/lib/black_jack.rb
@@ -1,11 +1,14 @@
 def bank_score
+	21-rand(6)
   # TODO: Use Random to get a new random score
 end
 
 def pick_card
+	1+rand(11)
   # TODO: Use Random to get a new random card
 end
 
 def game_outcome(bank, score)
+	return [bank, score]
   # TODO: Take the bank and the score and output an array containing the bank and then the score
 end
--- 01-Ruby/02-Arrays-Enumerable/03-Black-Jack/lib/croupier.rb
+++ 01-Ruby/02-Arrays-Enumerable/03-Black-Jack/lib/croupier.rb
@@ -1,17 +1,44 @@
 require_relative 'black_jack'
 
 def play_game
-  #TODO: make the user play from terminal in a while loop that will stop when the user will not be asking for  a new card
+	
+
+
+
+	answer = "y"
+	while answer == "y" || answer == "yes"
+		print "Do you want antother card ?"
+		answer = gets.chomp
+	end
 end
 
+  #TODO: make the user play from terminal in a while loop that will stop when the user will not be asking for  a new card
+
 def state_of_the_game(score, bank)
+	"Your score is #{score} and the bank is #{bank}"
   # TODO: Returns custom message with player's score and bank's score
 end
 
+
 def asking_for_card?(score)
+		print "Cards ?"
+		answer = gets.chomp
+		if answer == "y" || answer == "yes" then return true
+		else return false
+		end
+
+
   # TODO: Ask for a card only if the score is less or equal to 21, then returns true or false according to the user's choice
 end
 
 def build_message_for(outcome)
-  #TODO: return specific message depending on the game outcome (= bank's score and user's score)
+  if outcome[1] == 21 then puts "Black Jack !  You win !"
+  elsif outcome[1] > 21 then puts "You loose !"
+  elsif outcome[1] >= outcome[0] then puts "You win !"
+  else puts "You loose !"
+  end
 end
+
+#TODO: return specific message depending on the game outcome (= bank's score and user's score)
+
+
