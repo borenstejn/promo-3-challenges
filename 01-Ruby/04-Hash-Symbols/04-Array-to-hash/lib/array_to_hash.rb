4d98defb0ebb6401184f2d1d5e6581a0e7a01492
borenstejn <jeromeborenstejn@gmail.com>
2014-07-11 16:21:27 +0200

    résolution array to hash

--- 01-Ruby/04-Hash-Symbols/04-Array-to-hash/lib/array_to_hash.rb
+++ 01-Ruby/04-Hash-Symbols/04-Array-to-hash/lib/array_to_hash.rb
@@ -1,3 +1,15 @@
 def array_to_hash(array)
-  #TODO: implement the method :)
+hash = Hash.new
+array.each_with_index {|item, index|
+  if block_given?
+    then hash[yield(index)] = item
+  else hash[index] = item
+  end
+}
+
+return hash
+
 end
+
+
+#TODO: implement the method :)
