476b73b3fc1314a074aab8f873ba9a30b609c781
borenstejn <jeromeborenstejn@gmail.com>
2014-07-15 16:04:15 +0200

    résolution numéro de téléphone

--- 01-Ruby/05-Regular-Expressions/01-Phone-regexp/lib/check_phone.rb
+++ 01-Ruby/05-Regular-Expressions/01-Phone-regexp/lib/check_phone.rb
@@ -1,3 +1,8 @@
 def french_phone_number?(phone_number)
+number=phone_number.delete " .-"
+if number =~ /(\b0[1-9]\d{8}\b)|(\+33[1-9]\d{8}\b)/
+    return true
+  else return false
   # TODO: true or false?
+  end
 end
