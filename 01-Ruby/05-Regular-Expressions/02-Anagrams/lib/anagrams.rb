084723b6b22dcb808d96c573863bb3c30e69f1ab
borenstejn <jeromeborenstejn@gmail.com>
2014-07-16 12:54:20 +0200

    a terminer

--- 01-Ruby/05-Regular-Expressions/02-Anagrams/lib/anagrams.rb
+++ 01-Ruby/05-Regular-Expressions/02-Anagrams/lib/anagrams.rb
@@ -1,8 +1,29 @@
 def anagrams?(a_string, another_string)
+  array = a_string.downcase.delete(" '-").chars.sort
+  another_array = another_string.delete(" '-").chars.sort
+  if array == another_array
+    return true
+    else return false
+  end
+
   #TODO: implement the obvious method to test if two words are anagrams
 end
 
 def anagrams_on_steroids?(a_string, another_string)
-  #TODO: implement the improved method
+  a_hash = Hash.new
+  another_hash = Hash.new
+  a_string.downcase.delete(" '-?!").each_char do |c|
+    a_hash [c] = a_string.downcase.delete(" '-?!").count(c)
+  end
+  another_string.downcase.delete(" '-?!").each_char do |c|
+    another_hash [c] = another_string.downcase.delete(" '-?!").count(c)
+  end
+  if a_hash == another_hash
+    then return true
+  else return false
+  end
 end
-  
+
+  #TODO: implement the improved methody
+
+
