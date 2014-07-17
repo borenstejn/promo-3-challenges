3e04a8bde5ff00608639adf468210d2aba59b154
borenstejn <jeromeborenstejn@gmail.com>
2014-07-17 15:26:34 +0200

    balck jack resolution

--- 01-Ruby/02-Arrays-Enumerable/03-Black-Jack/lib/croupier.rb
+++ 01-Ruby/02-Arrays-Enumerable/03-Black-Jack/lib/croupier.rb
@@ -1,7 +1,7 @@
 require_relative 'black_jack'
 
 def play_game
-	
+
 
 
 
@@ -15,7 +15,7 @@ end
   #TODO: make the user play from terminal in a while loop that will stop when the user will not be asking for  a new card
 
 def state_of_the_game(score, bank)
-	"Your score is #{score} and the bank is #{bank}"
+	"Your score is #{score}, bank is #{bank}!"
   # TODO: Returns custom message with player's score and bank's score
 end
 
@@ -23,19 +23,24 @@ end
 def asking_for_card?(score)
 		print "Cards ?"
 		answer = gets.chomp
-		if answer == "y" || answer == "yes" then return true
+		if answer == "y" || answer == "yes"
+      return true
 		else return false
 		end
+  end
 
 
   # TODO: Ask for a card only if the score is less or equal to 21, then returns true or false according to the user's choice
-end
+
 
 def build_message_for(outcome)
-  if outcome[1] == 21 then puts "Black Jack !  You win !"
-  elsif outcome[1] > 21 then puts "You loose !"
-  elsif outcome[1] >= outcome[0] then puts "You win !"
-  else puts "You loose !"
+  if outcome[1] == 21
+    return "Black Jack!"
+  elsif outcome[1] > 21
+    return "You are over 21... you loose."
+  elsif outcome[1] > outcome[0]
+    return "You beat the bank! You win."
+  else return "Bank beats you! You loose."
   end
 end
 
