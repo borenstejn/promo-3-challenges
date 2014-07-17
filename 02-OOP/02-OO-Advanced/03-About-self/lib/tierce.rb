72db8a3780e075bb1dea7ddd6d617504f90db599
borenstejn <jeromeborenstejn@gmail.com>
2014-07-17 14:08:33 +0200

    résolution exercice about self

--- 02-OOP/02-OO-Advanced/03-About-self/lib/tierce.rb
+++ 02-OOP/02-OO-Advanced/03-About-self/lib/tierce.rb
@@ -1,18 +1,21 @@
 module DemoModule
 
-  class DemoClass  
-    
+  class DemoClass
+
     def self.introduce
+      return 2
       #TODO: choose return between 1, 2, 3
     end
 
     def introduce
+      return 3
       #TODO: choose return between 1, 2, 3
     end
 
   end
-  
+
   def self.introduce
+    return 1
     #TODO: choose return between 1, 2, 3
   end
 
@@ -20,7 +23,7 @@ end
 
 def has_tierce?
   first = DemoModule::introduce
-  second = DemoModule::DemoClass.introduce      
+  second = DemoModule::DemoClass.introduce
   third = DemoModule::DemoClass.new.introduce
   [first, second, third] == [1, 2, 3]
 end
