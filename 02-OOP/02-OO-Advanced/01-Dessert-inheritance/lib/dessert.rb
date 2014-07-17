41ce4bf6e99e64214e5b862bc9b438d3226772b4
borenstejn <jeromeborenstejn@gmail.com>
2014-07-17 11:10:25 +0200

    résolution exercice inheritance

--- 02-OOP/02-OO-Advanced/01-Dessert-inheritance/lib/dessert.rb
+++ 02-OOP/02-OO-Advanced/01-Dessert-inheritance/lib/dessert.rb
@@ -1,15 +1,39 @@
 class Dessert
+
+attr_accessor :name, :calories
+
   def initialize(name, calories)
-  end 
-  
-  def healthy?
+    @name = name
+    @calories = calories
   end
-  
+
+  def healthy?
+    if @calories < 200
+      return true
+    else return false
+    end
+  end
+
   def delicious?
+    true
   end
 end
 
-class JellyBean < Dessert  
+class JellyBean < Dessert
+
+attr_reader :flavor
+
   def initialize(name, calories, flavor)
+    @name = name
+    @calories = calories
+    @flavor = flavor
   end
+
+  def delicious?
+    if @flavor == "black licorice"
+      return false
+    else return true
+    end
+  end
+
 end
