71b47216a3c25fb32748bd96fe3e57213a51e62a
borenstejn <jeromeborenstejn@gmail.com>
2014-07-19 13:49:59 +0200

    résolution PMU

--- 01-Ruby/02-Arrays-Enumerable/06-Destructive-methods/lib/pmu.rb
+++ 01-Ruby/02-Arrays-Enumerable/06-Destructive-methods/lib/pmu.rb
@@ -1,7 +1,10 @@
 def pmu_format!(race_array)
+  race_array.each_with_index do |item, index|
+     race_array[index] = "#{index+1}-#{item}!"
+  end
   #TODO: modify the given array so that it is PMU-consistent
+  return race_array.reverse!
 end
-
 
 
 
