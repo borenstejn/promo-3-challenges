a1cc91415157ecbcb45432879c29edd28792d912
borenstejn <jeromeborenstejn@gmail.com>
2014-07-28 11:25:49 +0200

    mise à jour et Migration

--- 03-AR-Database/02-SQL-CRUD/lib/crud.rb
+++ 03-AR-Database/02-SQL-CRUD/lib/crud.rb
@@ -2,31 +2,48 @@ require 'sqlite3'
 
 def create_scheme(db)
   #TODO: create your db scheme
-  sql = %q{ 
-  }  # %q allows for multiline string in ruby 
+  sql = %q{CREATE TABLE IF NOT EXISTS posts
+(
+  id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+  name VARCHAR(100),
+  rating INT TYPE DEFAULT 2,
+  source_url VARCHAR(300),
+  date DATETIME
+)
+  }  # %q allows for multiline string in ruby
   db.execute(sql)
 end
 
 def create_post(db, post)
+  db.execute("INSERT INTO posts(name, source_url, date, rating)
+  VALUES ('#{post[:name]}', '#{post[:source_url]}', '#{post[:date]}', #{post[:rating]})")
   #TODO: add a new post to your db
 end
 
 def get_posts(db)
+  puts db.execute("SELECT * FROM posts ")
    #TODO: list all posts
 end
 
 def get_post(db, id)
+  puts db.execute("SELECT * FROM posts
+  WHERE id = #{id}")
   #TODO: get a specific post
 end
 
 def update_post(db, id, name)
+  db.execute("UPDATE posts SET name = '#{name}'
+  WHERE id = #{id}")
 	#TODO: update a post's name in your db
 end
 
 def delete_posts(db)
+  db.execute("DELETE FROM posts")
   #TODO: delete a post in your db
 end
 
 def delete_post(db, id)
+  db.execute("DELETE FROM posts
+  WHERE id = #{id}")
   #TODO: delete a specific post in your db
 end
--- 03-AR-Database/04-SQL-queries/lib/join_queries.rb
+++ 03-AR-Database/04-SQL-queries/lib/join_queries.rb
@@ -1,11 +1,11 @@
 require 'sqlite3'
 
 # opens the database
-DATABASE_PATH = "lib/db/jukebox.sqlite"
+DATABASE_PATH = "db/jukebox.sqlite"
 db = SQLite3::Database.new(DATABASE_PATH)
 
 def detailed_tracks(db)
-  db.execute("SELECT t.Name, art.Name, alb.Title
+  db.execute("SELECT t.Name, art.Name, alb.Name
     FROM Track t
     JOIN Album alb ON t.AlbumID = alb.AlbumID
     JOIN Artist art ON alb.ArtistID = art.ArtistID")
@@ -13,35 +13,11 @@ def detailed_tracks(db)
 end
 
 def stats_on(db, category)
-  array = db.execute("SELECT COUNT(*), AVG(Milliseconds)
-                                FROM Track t
-                                JOIN Genre g ON t.GenreId =  g.GenreId
-                                WHERE g.Name = #{category}")
   #TODO: For the given category of music, return the number of tracks and the average song length (as a stats hash)
-
-
-  hash {
-    category: category
-    number_of_tracks: array[0]
-    average_length: array[1]
-  }
-
-  return hash
-
-
-
 end
-
-
 
 def top_five_rock_artists(db)
   #TODO: return list of top 5 rock artists
 end
 
 puts detailed_tracks(db)
-
-
-
-
-
-SELECT Genre, AVG(Milliseconds)
