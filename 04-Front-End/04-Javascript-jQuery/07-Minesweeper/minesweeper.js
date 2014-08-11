a2d1adccb753f08519e467b80383d749076927c0
borenstejn <jeromeborenstejn@gmail.com>
2014-08-11 12:17:36 +0200

    minesweeper

--- 04-Front-End/04-Javascript-jQuery/07-Minesweeper/minesweeper.js
+++ 04-Front-End/04-Javascript-jQuery/07-Minesweeper/minesweeper.js
@@ -1,1 +1,26 @@
 // Good luck!
+var num=1;
+var neighboor;
+
+$('td').click(function(){
+  var col = $(this).parent().children().index($(this));
+  var row = $(this).parent().parent().children().index($(this).parent());
+
+  $(this)
+
+  $('((tbody tr:nth-child('+num+')) td:nth-child(1)').addClass('test');
+
+  // $(this).parent().attr('id','test');
+
+
+
+
+
+  // $('tbody tr:nth-child('+num+')').addClass('test');
+  console.debug(num);
+  console.debug($(this).parent().parent().children().index($(this).parent()));
+  // console.debug($(this).parent().children().index(2));
+});
+
+
+// $('.mined).addClass('test')
